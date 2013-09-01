3976c078f268453697bc865845ab2c2254ec88b8
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2013-09-01 18:28:42 -0300

    Update urlpatters and imports

--- urls.py
+++ urls.py
@@ -1,9 +1,10 @@
+#-*- coding:utf-8 -*-
+
+from django.conf.urls import patterns, url, include
+from django.core.urlresolvers import reverse_lazy
 from django.conf import settings
-from django.conf.urls.defaults import include, patterns
 
-from {{ app_name }}.views import hello
-
-
-urlpatterns = patterns('',
-    (r'^hello/$', hello),
+urlpatterns = patterns(
+    {{ app_name }}.views,
+    url(r'^$', 'homepage', name='homepage'),
 )
