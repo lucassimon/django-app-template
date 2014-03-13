4e630a742199df96e6f0c0ce1127a4df4f40540c
Lucas Simon <lucassrod@gmail.com>
2014-03-12 22:36:19 -0300

    Atualiza o model com padroes de comentario

--- models.py
+++ models.py
@@ -13,41 +13,147 @@ class TimeStampedModel(models.Model):
     """
 
     created = models.DateTimeField(auto_now_add=True)
+    """
+    Atributo da classe TimeStampedModel para
+    referenciar um campo de data de criação
+
+    Caracteristicas:
+    DateTimeField
+    auto now add: True
+    """
+
     modified = models.DateTimeField(auto_now=True)
+    """
+    Atributo da classe TimeStampedModel para
+    referenciar um campo de data de modificado
+
+    Caracteristicas:
+    DateTimeField
+    auto now: True
+    """
 
     class Meta:
+        """
+        Seta a classe como abstrata
+        """
         abstract = True
 
 
-class Example(TimeStampedModel):
+class StandardItemStuffModel(models.Model):
     """
-    An example class model to standardize
-    new models of project.
+    Classe abstrata para comportar campos em comum
+    com os as apps de palestras/talks,
+    screencasts, tutoriais/artigos,
     """
 
-    _STATUS_MAX_LENGTH = 1
-    _STATUS_DEFAULT = 'D'
-    _STATUS_CHOICES = (
-        ('A', _(u'Ativo')),
-        ('B', _(u'Bloqueado')),
-        ('C', _(u'Cancelado')),
-        (_STATUS_DEFAULT, _(u'Em aprovação')),
-        ('E', _(u'Inativo')),
+    speaker = models.ForeignKey(
+        'speakers.Speaker',
+        verbose_name=_(u'Palestrante'),
+    )
+    """
+    Atributo da classe StandardItemStuffModel para
+    referenciar um objeto da classe speaker
+
+    Caracteristicas:
+    ForeignKey
+    verbose name: palestrante
+    """
+
+    title = models.CharField(
+        verbose_name=_(u'Titulo'),
+        max_length=255
+    )
+    """
+    Atributo da classe StandardItemStuffModel para
+    setar um titulo
+
+    Caracteristicas:
+    CharField
+    verbose name: Titulo
+    max length: 255
+    """
+
+    slug = models.SlugField(
+        verbose_name=_(u'Slug'),
+        unique=True,
+        null=True
     )
 
-    title = models.CharField(_(u'Titulo'), max_length=255)
-    slug = models.SlugField(_(u'Slug'), unique=True)
-    status = models.CharField(
-        _(u'Status'),
-        choices=_STATUS_CHOICES,
-        default=_STATUS_DEFAULT,
-        max_length=_STATUS_MAX_LENGTH
+    summary = models.TextField(
+        verbose_name=_(u'Sumário')
     )
+    """
+    Atributo da classe StandardItemStuffModel para
+    setar um sumário
+
+    Caracteristicas:
+    TextField
+    verbose name: Sumário
+    """
 
     class Meta:
-        ordering = ['created']
-        verbose_name = _(u'Example')
-        verbose_name_plural = _(u'Examples')
+        """
+        Seta a classe como abstrata
+        """
+        abstract = True
 
-    def __unicode__(self):
-        return u'%s' % (self.title)
+
+class Media(TimeStampedModel):
+    """
+    Classe model para cadastrar as medias
+    com os as apps de palestras/talks,
+    screencasts, tutoriais/artigos,
+    """
+
+    _KIND_MEDIAS = (
+        ('TU', _(u'Tutorial')),
+        ('CD', _(u'Codigo')),
+        ('VI', _(u'Video')),
+        ('SL', _(u'Slide')),
+    )
+
+    type = models.CharField(
+        verbose_name=_(u'Tipo'),
+        max_length=3,
+        choices=_KIND_MEDIAS
+    )
+    """
+    Atributo da classe Media para escolher as
+    opcoes setada na tupla KINDS
+
+    Caracteristicas:
+    CharField
+    max length: 2
+    """
+
+    title = models.CharField(
+        verbose_name=_(u'Titulo'),
+        max_length=255
+    )
+    """
+    Atributo da classe Media para
+    setar um titulo
+
+    Caracteristicas:
+    CharField
+    verbose name: Titulo
+    max length: 255
+    """
+
+    url = models.URLField(
+        verbose_name=_('URL'),
+    )
+    """
+    Atributo da classe Media para
+    setar uma url valida
+
+    Caracteristicas:
+    UrlField
+    verbose name: URL
+    """
+
+    class Meta:
+        """
+        Seta a classe como abstrata
+        """
+        abstract = True
