36604e99b642c32f98c49fc00c0200303961e266
Lucas Simon <lucassrod@gmail.com>
2015-12-08 21:04:08 -0200

    Update template

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
-
+from __future__ import unicode_literals
 # Stdlib imports
 
 # Core Django imports
 from django.db import models
 from django.utils.translation import ugettext as _
+from django.core.urlresolvers import reverse
 # Third-party app imports
 
 # Imports from your apps
--- views.py
+++ views.py
@@ -1,10 +1,14 @@
 # -*- coding:utf-8 -*-
+from __future__ import unicode_literals
 
 # Stdlib imports
 
 # Core Django imports
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.generic import CreateView, TemplateView, ListView, DetailView
+from django.core.urlresolvers import reverse_lazy
+from django.utils.translation import ugettext as _
 
 # Third-party app imports
 
